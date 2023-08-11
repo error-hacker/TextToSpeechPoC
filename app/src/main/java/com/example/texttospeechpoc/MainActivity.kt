05f6eb3f27ca0f53010868aebf9fc995f1ce3800
Sahil5 Gupta <mahajan000@hotmail.com>
2023-08-11 10:06:11 +0530

    next commit

--- app/src/main/java/com/example/texttospeechpoc/MainActivity.kt
+++ app/src/main/java/com/example/texttospeechpoc/MainActivity.kt
@@ -2,19 +2,19 @@ package com.example.texttospeechpoc
 
 import android.Manifest.permission.RECORD_AUDIO
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
 import com.example.texttospeechpoc.databinding.ActivityMainBinding
 import java.util.*
 
@@ -23,8 +23,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var textToSpeech1 : TextToSpeech
-    private lateinit var speechRecognizer : SpeechRecognizer
-    private lateinit var speechRecognizerIntent : Intent
 
     private var currentSpeechLanguage = "en_IN"
 
@@ -40,7 +38,6 @@ class MainActivity : AppCompatActivity() {
         )
 
         initTextToSpeech()
-        initSpeechToText()
         initRadioGroup()
 
 
@@ -51,24 +48,45 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.ivMic.setOnClickListener {
-            checkPermission(
-                RECORD_AUDIO,
-                MIC_PERMISSION_CODE
-            )
-            speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, currentSpeechLanguage)
-            speechRecognizer.startListening(speechRecognizerIntent)
+//            checkPermission(
+//                RECORD_AUDIO,
+//                MIC_PERMISSION_CODE
+//            )
+            initSpeechToText()
         }
-
-
     }
+
+    private val launcherGoogleTextToSpeech =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            when (it.resultCode) {
+                Activity.RESULT_OK -> {
+                    if (it != null && it.data != null
+                    ) {
+                        val matches =
+                            it.data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                        val sTextFromSpeech : String =
+                            if (!matches.isNullOrEmpty()) {
+                                matches[0].toString()
+                            } else {
+                                ""
+                            }
+                        binding.etStt.setText(sTextFromSpeech)
+                        Log.i(TAG, "onResults: $sTextFromSpeech")
+                    }
+                }
+                Activity.RESULT_CANCELED -> {
+
+                }
+            }
+        }
 
     private fun initRadioGroup() {
         binding.rgLanguages.setOnCheckedChangeListener { group, checkedId ->
             currentSpeechLanguage = when (checkedId) {
                 R.id.radioButton -> "en_IN"
-                R.id.radioButton2 -> "hi_IN"
-                R.id.radioButton3 -> "mr_IN"
-                R.id.radioButton4 -> "gu_IN"
+                R.id.radioButton2 -> "hi"
+                R.id.radioButton3 -> "mr"
+                R.id.radioButton4 -> "gu"
                 R.id.radioButton5 -> "kn_IN"
                 R.id.radioButton6 -> "te_IN"
                 else -> ""
@@ -78,84 +96,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initSpeechToText() {
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        if(SpeechRecognizer.isRecognitionAvailable(this)) {
+           val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
 
-        speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
+            speechRecognizerIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            speechRecognizerIntent.putExtra(
+                RecognizerIntent.EXTRA_PROMPT,
+                "Say Something"
+            )
+            speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, currentSpeechLanguage)
+            launcherGoogleTextToSpeech.launch(speechRecognizerIntent)
 
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
-        speechRecognizerIntent.putExtra(
-            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-        )
-
-        speechRecognizer.setRecognitionListener(
-            object : RecognitionListener {
-                override fun onReadyForSpeech(params : Bundle?) {
-                    DrawableCompat.setTint(
-                        DrawableCompat.wrap(binding.ivMic.drawable),
-                        ContextCompat.getColor(this@MainActivity, R.color.red)
-                    )
-                    Log.i(TAG, "onReadyForSpeech")
-                }
-
-                override fun onBeginningOfSpeech() {
-                    Log.i(TAG, "onBeginningOfSpeech")
-                }
-
-                override fun onRmsChanged(rmsdB : Float) {
-                }
-
-                override fun onBufferReceived(buffer : ByteArray?) {
-                }
-
-                override fun onEndOfSpeech() {
-                    Log.i(TAG, "onEndOfSpeech")
-                }
-
-                override fun onError(error : Int) {
-                    DrawableCompat.setTint(
-                        DrawableCompat.wrap(binding.ivMic.drawable),
-                        ContextCompat.getColor(this@MainActivity, R.color.purple_700)
-                    )
-                    Log.i(TAG, "onError: $error")
-                }
-
-                override fun onResults(results : Bundle?) {
-                    DrawableCompat.setTint(
-                        DrawableCompat.wrap(binding.ivMic.drawable),
-                        ContextCompat.getColor(this@MainActivity, R.color.purple_700)
-                    )
-
-                    val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                    val sTextFromSpeech : String =
-                        if (!matches.isNullOrEmpty()) {
-                            matches[0].toString()
-                        } else {
-                            ""
-                        }
-                    binding.etStt.setText(sTextFromSpeech)
-                    Log.i(TAG, "onResults: $sTextFromSpeech")
-                }
-
-                override fun onPartialResults(partialResults : Bundle?) {
-                    val data = partialResults?.getStringArrayList(
-                        SpeechRecognizer.RESULTS_RECOGNITION
-                    )
-
-                    val sTextFromSpeech : String = if (data != null) {
-                        data[0].toString()
-                    } else {
-                        ""
-                    }
-                    binding.etStt.setText(sTextFromSpeech)
-                    Log.i(TAG, "onPartialResults: $sTextFromSpeech")
-                }
-
-                override fun onEvent(eventType : Int, params : Bundle?) {
-                    Log.i(TAG, "onEvent: $eventType")
-                }
-            }
-        )
+        }
     }
 
     private fun speak() {
@@ -179,7 +135,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         textToSpeech1.shutdown()
-        speechRecognizer.destroy()
         super.onDestroy()
     }
 
@@ -215,7 +170,6 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val TAG = "STT"
-        private const val MIC_PERMISSION_CODE = 100
     }
 
 }
